14630aefc3656afb97bc6ca37343086e760f5a23
Ian Gabriel Gallardo <ian01gallardo@gmail.com>
2022-09-15 18:38:58 -0300

    .

--- src/App.js
+++ src/App.js
@@ -1,10 +1,6 @@
-  import './App.css';
+import './App.css';
 import {BrowserRouter as Router, Switch, Route, Routes} from 'react-router-dom'
-// import {FunkoM} from './components/props/props';
 import Navbar from './components/navegate/Navbar'
-// import Inicio from './components/pages/Inicio'
-// import Funko from './components/pages/Funko'
-// import Items from './components/pages/Items'
 import {ItemListCointainer} from './components/ItemListCointainer/index';
 import {ItemDetailContainer} from './components/ItemDetailContainer/ItemDetailContainer';
 
